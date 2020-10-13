db39dc7428b3908968528e2394ad10350a4ae8dc
Mikael <mika.fr99hotmail.com>
2020-10-13 15:27:03 +0200

    Player base mechanics
    - Player Movements responding to keyboard inputs (
    - Player basic Combo
    - Player Placeholder animations

---
+++ Assets/Assets/Character/Scripts/CharacterMovement.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    //Linked components
+    private CharacterController2D cc2d; //The Movement controller attached on player
+    private Animator animator; // The animator attached on the player
+
+    private int direction = 0; //Value indicating whether the player is moving to the right, to the left or is just static
+    [SerializeField] private float acceleration;
+
+    private float horizontalMove = 0.0f;
+    private float lastValue;
+
+    private bool jump = false;
+
+    // Awake is called once before Start
+    void Awake()
+    {
+        cc2d = GetComponent<CharacterController2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyboardKeyDetection();
+        Actions();
+        applyMovement();
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+    }
+
+    void FixedUpdate()
+    {
+        cc2d.Move(horizontalMove, jump);
+    }
+
+    //Detects which keyboard movement keys are actually pressed and returns the value corresponding
+    // -1 for left / 1 for right / 0 for static
+    void KeyboardKeyDetection()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 0;
+            cc2d.stopCharacter();
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = 0;
+            cc2d.stopCharacter();
+        }
+        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = 0;
+        }
+    }
+
+    //Movements to apply depending on which controller is used
+    void applyMovement()
+    {
+        horizontalMoveResult();
+        ControllerMovements();
+    }
+
+    void horizontalMoveResult()
+    {
+        horizontalMove = direction;
+    }
+
+    void ControllerMovements()
+    {
+        if (Input.GetAxisRaw("Horizontal") < 0 && lastValue > 0 
+            || Input.GetAxisRaw("Horizontal") > 0 && lastValue < 0)
+        {
+            cc2d.stopCharacter();
+        }
+        if(Input.GetAxisRaw("Horizontal") >= 0.05)
+        {
+            horizontalMove = 1 * acceleration;
+        }
+        else if(Input.GetAxisRaw("Horizontal") <= -0.05)
+        {
+            horizontalMove = -1 * acceleration;
+        }
+        lastValue = horizontalMove;
+
+    }
+
+
+    //Character actions
+    void Actions()
+    {
+        if (Input.GetButtonDown("Jump") && !jump)
+        {
+            jump = true;
+            animator.SetBool("jump", true);
+        }
+    }
+
+    public void OnLanding()
+    {
+        Debug.Log("OnLanding");
+        animator.SetBool("jump", false);
+        cc2d.ResetJump();
+        jump = false;
+    }
+
+    //jump getter
+    public bool isJumping()
+    {
+        return jump;
+    }
+
+    //direction getter
+    public int getDirection()
+    {
+        return direction;
+    }
+}
---
+++ Assets/Assets/Character/Scripts/CombatManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CombatManager : MonoBehaviour
+{
+    public bool canReceiveInput = true;                 // For determining if the character is able to receive an input 
+    public bool inputReceived;                          // Checks if an attackInput has been pressed down
+    private CharacterMovement CM;                       
+    private CharacterController2D CC2d;
+    [SerializeField] private float AttackMoveForce;
+
+
+    public static CombatManager CMInstance;
+
+    void Awake()
+    {
+        CMInstance = this;
+        CM = GetComponent<CharacterMovement>();
+        CC2d = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attack();
+    }
+
+    public void Attack()
+    {
+        if (Input.GetButtonDown("Fire1") && !CM.isJumping())
+        {
+            if (canReceiveInput)
+            {
+                CC2d.getRigidbody().AddForce(new Vector2(CC2d.facingDirection() * AttackMoveForce, 0), ForceMode2D.Impulse);
+                canReceiveInput = false;
+                inputReceived = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
