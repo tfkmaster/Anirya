803c364cb3ab6ab193712ed61dd7da1dd533fa15
Eddy <eddy.abada.ea@gmail.com>
2020-10-23 16:06:26 +0200

    Add bouncy platforms logic

---
+++ Assets/BouncyManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyManager : MonoBehaviour
+{
+    [SerializeField]
+    private float bouncy_amount = 26;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
+        if(collision.gameObject.tag == "Player")
+        {
+            Player.GetComponent<Animator>().SetBool("jump", false);
+            Player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bouncy_amount, ForceMode2D.Impulse);
+        }
+    }
+}
