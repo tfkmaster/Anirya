758d329fd120229f1da8e5fcf844e6e4a665fc69
Eddy <eddy.abada.ea@gmail.com>
2020-10-30 17:17:25 +0100

    dialog system (no trigger, no display)

---
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting Conversation with " + dialogue.name);
+
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+
+    }
+}
