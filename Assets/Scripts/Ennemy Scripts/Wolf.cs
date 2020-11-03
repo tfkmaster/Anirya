4870eb3603460fd88753fe9356f16bded88fb8f4
Mikael <mika.fr99hotmail.com>
2020-11-03 19:07:14 +0100

    Wolf AI Started

---
+++ Assets/Scripts/Ennemy Scripts/Wolf.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wolf : Ennemy
+{
+    public List<GameObject> WanderNodes;
+    public List<GameObject> accessibleNodes;
+    public GameObject DestinationNode;
+    public GameObject StoredNode;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < WanderNodes.Count; i++)
+        {
+            accessibleNodes.Add(WanderNodes[i]);
+        }
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void OnHit(GameObject hitter, int damages)
+    {
+        base.OnHit(hitter, damages);
+    }
+
+    public void SelectANode()
+    {
+        DestinationNode = accessibleNodes[Random.Range(0, accessibleNodes.Count)];
+    }
+}
---
+++ Assets/WolfWander.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfWander : StateMachineBehaviour
+{
+
+    private Wolf wolfScript;
+    private Rigidbody2D rb2dWolf;
+
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        wolfScript = animator.gameObject.GetComponent<Wolf>();
+        rb2dWolf = animator.gameObject.GetComponent<Rigidbody2D>();
+
+
+    }
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Mathf.Abs(animator.transform.position.x) - Mathf.Abs(wolfScript.DestinationNode.transform.position.x) <= 0.2f)
+        {
+            if (animator.transform.position.x <= wolfScript.DestinationNode.transform.position.x)
+            {
+                rb2dWolf.velocity = new Vector2(wolfScript.speed, rb2dWolf.velocity.y);
+            }
+            else
+            {
+                rb2dWolf.velocity = new Vector2(-wolfScript.speed, rb2dWolf.velocity.y);
+            }
+        }
+        else
+        {
+
+        }
+        
+            
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
