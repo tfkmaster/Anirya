69e3d5fd3777c251f28efd8f7f535ed6c40bfc15
Mikael <mika.fr99hotmail.com>
2020-11-03 18:32:41 +0100

    Node on platform edges algorithm

---
+++ Assets/IAPlatformController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAPlatformController : MonoBehaviour
+{
+    void Awake()
+    {
+        transform.GetChild(0).transform.localPosition = new Vector3(
+            -GetComponent<SpriteRenderer>().sprite.bounds.extents.x * transform.GetChild(0).transform.localScale.x - 0.1f,
+            GetComponent<SpriteRenderer>().sprite.bounds.extents.y * transform.GetChild(0).transform.localScale.y + 0.3f,
+            transform.GetChild(0).transform.position.z);
+
+        transform.GetChild(1).transform.localPosition = new Vector3(
+            GetComponent<SpriteRenderer>().sprite.bounds.extents.x * transform.GetChild(0).transform.localScale.x + 0.1f,
+            GetComponent<SpriteRenderer>().sprite.bounds.extents.y * transform.GetChild(0).transform.localScale.y + 0.3f,
+            transform.GetChild(0).transform.position.z);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/NodeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeController : MonoBehaviour
+{
+    public List<GameObject> neighbours = new List<GameObject>();
+
+    [SerializeField] private LayerMask WhatIsGround;                          // A mask determining what is ground to detect further raycast collision
+    [SerializeField] private LayerMask WhatIsNodeLayer;                          // A mask determining in which layer the nodes are
+
+    public float collisionRadius = 0.12f;
+    private Color debugCollisionColor = Color.blue;
+
+    public Collider2D[] test;
+
+    void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LateStart(0.5f));
+    }
+
+    IEnumerator LateStart(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        test = Physics2D.OverlapCircleAll(transform.position, collisionRadius, WhatIsNodeLayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = debugCollisionColor;
+        Gizmos.DrawWireSphere(transform.position, collisionRadius);
+    }
+}
