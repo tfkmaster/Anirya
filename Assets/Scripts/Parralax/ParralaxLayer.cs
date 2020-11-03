20a0cf7a34ba8ef27cbb6dbfa5449b28447fa0e7
Eddy <eddy.abada.ea@gmail.com>
2020-11-03 15:24:16 +0100

    assets integrated for testing

---
+++ Assets/Scripts/Parralax/ParralaxLayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ParralaxLayer : MonoBehaviour
+{
+    [SerializeField] float multiplier = 0.0f;
+    [SerializeField] bool horizontalOnly = true;
+
+    private Transform cameraTransform;
+
+    
+    void Start()
+    {
+        
+    }
+
+
+    private void LateUpdate()
+    {
+        
+    }
+}
