7c5bcbf3a6e0ac6b2662d2a05ad3f00f5be9e016
Mikaël <mika.fr99@hotmail.com>
2020-11-25 19:31:52 +0100

    Add FX to wall hit

--- Assets/Scripts/Environement Interactions/DestructibleWall.cs
+++ Assets/Scripts/Environement Interactions/DestructibleWall.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class DestructibleWall : Actor
 {
+    public ParticleSystem onHitParticles1;
+    public ParticleSystem onHitParticles2;
     protected override void Death()
     {
+        Destroy(GetComponent<BoxCollider2D>());
         GetComponent<Animator>().SetBool("dead", true);
     }
 
     public override void OnHit(GameObject hitter, int damages)
     {
+        onHitParticles1.Play();
+        onHitParticles2.Play();
         base.OnHit(hitter, damages);
         GetComponent<Animator>().SetTrigger("hit");
         Debug.Log("Wall hit");
