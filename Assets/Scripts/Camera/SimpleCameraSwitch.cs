bda2a8b382e707556b2c4b3fb8098285884ed649
Eddy <eddy.abada.ea@gmail.com>
2020-12-13 15:40:00 +0100

    Scene transition + fusion s002 & s003

--- Assets/Scripts/Camera/SimpleCameraSwitch.cs
+++ Assets/Scripts/Camera/SimpleCameraSwitch.cs
@@ -13,9 +13,31 @@ public class SimpleCameraSwitch : MonoBehaviour
     [SerializeField]
     private Transform CSTrigger = default;
 
+    private Vector3 target_cam_pos; 
+
     void Start()
     {
         Anirya = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        if(SourceCam.Follow == null && SourceCam.CompareTag("FollowCamera"))
+        {
+            SourceCam.Follow = Anirya;
+        }
+        else if(TargetCam.Follow == null && TargetCam.CompareTag("FollowCamera"))
+        {
+            TargetCam.Follow = Anirya;
+        }
+
+        if (CSTrigger.position.x < Anirya.position.x)
+        {
+            SwitchCamera(true);
+        }
+        else if (CSTrigger.position.x >= Anirya.position.x)
+        {
+            SwitchCamera(false);
+        }
+
+        target_cam_pos = TargetCam.transform.position;
     }
 
     void Update()
