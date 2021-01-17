75c330b948458aab904d9928053233a41563ad97
Mikaël <mika.fr99@hotmail.com>
2021-01-17 08:19:06 +0100

    Anirya Foot align on slopes Idle

--- Assets/Scripts/Player Scripts/Player States/IdleBehavior.cs
+++ Assets/Scripts/Player Scripts/Player States/IdleBehavior.cs
@@ -17,6 +17,7 @@ public class IdleBehavior : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        animator.gameObject.GetComponentInParent<CharacterController2D>().root.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, animator.gameObject.GetComponentInParent<CharacterController2D>().root.transform.localRotation.z + 0.01f));
         if (CombatManager.CMInstance.inputReceived)
         {
             animator.SetTrigger("attack1");
@@ -37,9 +38,10 @@ public class IdleBehavior : StateMachineBehaviour
     //    // Implement code that processes and affects root motion
     //}
 
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+    //OnStateIK is called right after Animator.OnAnimatorIK()
+    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("afqsqdqsdssf");
+        // Implement code that sets up animation IK (inverse kinematics)
+    }
 }
