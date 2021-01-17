d312abc95c6e63ada702b5516d371eacc6aa0958
Valentin MARCHAL <ardrentel@gmail.com>
2021-01-17 08:33:17 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:Assets/Scripts/AudioSettings.cs
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -43,3 +44,56 @@ public class AudioSettings : MonoBehaviour
 
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AudioSettings : MonoBehaviour
+{
+     void Awake () 
+  {
+      
+  }
+   void Update () 
+  {
+     
+  }
+   public void AniryaWalking()
+    {
+
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Characters/Anirya Footsteps", GetComponent<Transform>().position);
+
+    }
+      public void AniryaJumpHead()
+    {
+
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Characters/Jump", GetComponent<Transform>().position);
+
+
+    }
+    public void AniryaJumpTail()
+    {
+
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Characters/Jump", GetComponent<Transform>().position);
+
+    }
+    public void AniryaHit()
+    {
+
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Characters/Punch", GetComponent<Transform>().position);
+
+    }
+    public void AniryaHeal()
+    {
+
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Characters/Heal", GetComponent<Transform>().position);
+
+    }
+    public void ShroomBump()
+    {
+
+        FMODUnity.RuntimeManager.PlayOneShot("event:/FX environement/Bumper", GetComponent<Transform>().position);
+
+    }
+}
+>>>>>>> Stashed changes:Assets/AudioSettings.cs
