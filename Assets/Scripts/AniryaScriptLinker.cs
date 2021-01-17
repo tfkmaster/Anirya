1204440e5dce4fcdbf4dceb977278e6d2ea306e9
Eddy <eddy.abada.ea@gmail.com>
2021-01-17 07:21:37 +0100

    Scene Management

---
+++ Assets/Scripts/AniryaScriptLinker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniryaScriptLinker : MonoBehaviour
+{
+    public CombatManager CombatM;
+    public Player Player;
+    public CharacterController2D CC2D;
+    public CharacterMovement CM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckHit()
+    {
+        CombatM.CheckHit();
+    }
+
+    /*public void EnableLeftFlame()
+    {
+        CC2D.EnableLeftFlame();
+    }
+    public void EnableRightFlame()
+    {
+        CC2D.EnableRightFlame();
+    }
+    public void DisableLeftFlame()
+    {
+        CC2D.DisableLeftFlame();
+    }
+
+    public void DisableRightFlame()
+    {
+        CC2D.DisableRightFlame();
+    }*/
+}
---
+++ Assets/Scripts/SceneManagement/tuto_002_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tuto_002_Manager : MonoBehaviour
+{
+    private ScenesManager PersistentDatas = default;
+    private Player player = default;
+    [SerializeField]
+    private GameObject trunk = default;
+    [SerializeField]
+    private GameObject tallam_shadows = default;
+    void Awake()
+    {
+        PersistentDatas = GameObject.FindGameObjectWithTag("PersistentDatas").GetComponent<ScenesManager>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Start()
+    {
+        if (PersistentDatas.tuto_002.TrunkIsBroken)
+        {
+            Destroy(trunk);
+        }
+
+        if (player.GM.alimMet && !PersistentDatas.tuto_003.FirstWolfSlayed)
+        {
+            tallam_shadows.GetComponent<ParticleSystem>().Play();
+            tallam_shadows.GetComponentInChildren<Collider2D>().isTrigger = false;
+        }
+    }
+
+    void Update()
+    {
+        if (!PersistentDatas.tuto_002.TrunkIsBroken && !trunk)
+        {
+            PersistentDatas.tuto_002.TrunkIsBroken = true;
+        }
+    }
+}
