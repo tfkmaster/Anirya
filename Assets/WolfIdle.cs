3e931cbea8320ea39528080506dc83b845663300
Mikaël <mika.fr99@hotmail.com>
2021-01-17 19:52:23 +0100

    Wolf Changes

--- Assets/WolfIdle.cs
+++ Assets/WolfIdle.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class WolfIdle : StateMachineBehaviour
 {
+    private float counter;
+    bool startCounter;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.gameObject.GetComponentInParent<Rigidbody2D>().velocity = Vector2.zero;
+        counter = 0;
+        startCounter = false;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if (startCounter)
+        {
+            counter += Time.deltaTime;
+            if(counter >= animator.gameObject.GetComponentInParent<Wolf>().waitTime)
+            {
+                animator.SetBool("playerGrounded", true);
+            }
+        }
+        
         if (animator.gameObject.GetComponentInParent<Wolf>().player.GetComponent<CharacterController2D>().m_Grounded)
         {
-            animator.SetBool("playerGrounded", true);
+            startCounter = true;
         }
         else
         {
