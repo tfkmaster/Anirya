808705a59ec6733a31d285feb25a50d5c098956b
Mikaël <mika.fr99@hotmail.com>
2021-02-15 21:00:36 +0100

    Folder management

--- Assets/Scripts/Ennemies/Wolf/Wolf States/WolfIdle.cs
+++ Assets/Scripts/Ennemies/Wolf/Wolf States/WolfIdle.cs
@@ -2,19 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Combo02End : StateMachineBehaviour
+public class WolfIdle : StateMachineBehaviour
 {
+    private float counter;
+    bool startCounter;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        animator.gameObject.GetComponentInParent<Rigidbody2D>().velocity = Vector2.zero;
+        counter = 0;
+        startCounter = false;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (startCounter)
+        {
+            counter += Time.deltaTime;
+            if(counter >= animator.gameObject.GetComponentInParent<Wolf>().waitTime)
+            {
+                animator.SetBool("playerGrounded", true);
+            }
+        }
+        
+        if (animator.gameObject.GetComponentInParent<Wolf>().player.GetComponent<CharacterController2D>().m_Grounded)
+        {
+            startCounter = true;
+        }
+        else
+        {
+            animator.SetBool("playerGrounded", false);
+        }
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- Assets/Scripts/Player Scripts/Player States/Jump.cs
+++ Assets/Scripts/Player Scripts/Player States/Jump.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Combo01End : StateMachineBehaviour
+public class Jump : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
