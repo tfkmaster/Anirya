4db4904abe015f60552bd10638b21272b44c98bf
Mikaël <mika.fr99@hotmail.com>
2021-03-07 16:57:02 +0100

    Anim V2

---
+++ Assets/AIWaghzen.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIWaghzen : MonoBehaviour
+{
+
+    public float GravityAfterJumpValue;
+
+    //Movement Information
+    public bool m_FacingRight = true;  // For determining which way the ennemy is currently facing.
+    public GameObject player;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        //Flip();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (player.transform.position.x < gameObject.transform.position.x && m_FacingRight)
+        {
+            Flip();
+        }
+
+        if (player.transform.position.x > gameObject.transform.position.x && !m_FacingRight)
+        {
+            Flip();
+        }
+
+        // If the ennemy is facing left and moves to the right...
+        if (GetComponentInParent<Rigidbody2D>() && GetComponentInParent<Rigidbody2D>().velocity.x > 0.5f && !m_FacingRight)
+        {
+            // ... flip the ennemy.
+            Flip();
+        }
+        // Otherwise if the ennemy is facing right and moves to the left...
+        else if (GetComponentInParent<Rigidbody2D>() && GetComponentInParent<Rigidbody2D>().velocity.x < -0.5f && m_FacingRight)
+        {
+            // ... flip the ennemy.
+            Flip();
+        }
+        if (GetComponentInChildren<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Wander"))
+        {
+            GetComponentInChildren<Animator>().SetTrigger("StartFollowing");
+        }
+    }
+
+    public void GravityBeforeJump()
+    {
+        GetComponent<Rigidbody2D>().gravityScale = 0;
+    }
+
+    public void GravityAfterJump()
+    {
+        GetComponent<Rigidbody2D>().gravityScale = GravityAfterJumpValue;
+    }
+
+    protected void Flip()
+    {
+        if (GetComponentInChildren<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Follow"))
+        {
+            // Switch the way the player is labelled as facing.
+            m_FacingRight = !m_FacingRight;
+
+            // Multiply the player's x local scale by -1.
+            Vector3 theScale = transform.GetChild(0).transform.localScale;
+            theScale.x *= -1;
+            transform.GetChild(0).transform.localScale = theScale;
+        }
+    }
+}
---
+++ Assets/WaghzenScriptLinker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaghzenScriptLinker : MonoBehaviour
+{
+    private AIWaghzen AIWaghzen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AIWaghzen = GetComponentInParent<AIWaghzen>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeforeJump()
+    {
+        AIWaghzen.GravityBeforeJump();
+    }
+
+    public void AfterJump()
+    {
+        AIWaghzen.GravityAfterJump();
+    }
+}
