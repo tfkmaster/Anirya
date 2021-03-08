4951a6adb6a52827224dae9177d32396e3dc3d8e
Eddy <eddy.abada.ea@gmail.com>
2021-03-08 19:13:15 +0100

    chap001 lights test

--- Assets/Scripts/SceneManagement/SceneHelpers.cs
+++ Assets/Scripts/SceneManagement/SceneHelpers.cs
@@ -6,12 +6,16 @@ public class SceneHelpers : MonoBehaviour
 {
     public GameObject GM;
     private GameObject GM_Instance;
+
+    public GameObject Alim;
+    private GameObject Alim_Instance;
     void Start()
     {
         #if UNITY_EDITOR
             if(GameObject.FindGameObjectWithTag("GameManager") == null)
             {
                 GM_Instance = Instantiate(GM, this.transform.position, new Quaternion(0, 0, 0, 0));
+                Alim_Instance = Instantiate(Alim, this.transform.position, new Quaternion(0, 0, 0, 0));
             }
         #endif
     }
