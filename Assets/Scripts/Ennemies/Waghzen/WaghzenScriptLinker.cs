5d1ffe5e31f3fa9791b72677b9a4ac44924afad7
Mikaël <mika.fr99@hotmail.com>
2021-03-11 18:02:05 +0100

    Waghzen Update V6

--- Assets/Scripts/Ennemies/Waghzen/WaghzenScriptLinker.cs
+++ Assets/Scripts/Ennemies/Waghzen/WaghzenScriptLinker.cs
@@ -20,7 +20,7 @@ public class WaghzenScriptLinker : MonoBehaviour
 
     public void Teleport()
     {
-        gameObject.GetComponentInParent<Transform>().position -= new Vector3(gameObject.GetComponentInParent<Transform>().position.x, gameObject.GetComponentInParent<Transform>().position.y -7, gameObject.GetComponentInParent<Transform>().position.z);
+        //gameObject.GetComponentInParent<Transform>().position = new Vector3(gameObject.GetComponentInParent<Transform>().position.x, gameObject.GetComponentInParent<Transform>().position.y - 40, gameObject.GetComponentInParent<Transform>().position.z);
     }
 
     public void BeforeJump()
