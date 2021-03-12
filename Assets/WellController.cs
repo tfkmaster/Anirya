b1a69316b6438866329304d8e469b814f310ddeb
Mikaël <mika.fr99@hotmail.com>
2021-03-12 02:05:30 +0100

    EndScreen

--- Assets/WellController.cs
+++ Assets/WellController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WellController : MonoBehaviour
 {
+    public Animator CameraAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,7 @@ public class WellController : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             GetComponent<Animator>().SetTrigger("Interact");
+            CameraAnimator.SetTrigger("Zoom");
         }
     }
 }
